b11ca825af7e6a6e369c0792e3973a83e1520f48
luke <luke@wemakeprice.com>
2020-09-09 15:47:06 +0900

    서버 > 클라이언트 데이터 전달

--- test-ssr/src/index.js
+++ test-ssr/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App'
 const initialData = window.__INITIAL_DATA__;
-console.log('initialData', initialData)
 // ReactDom.render(<App page="home" />, document.getElementById('root'));
 ReactDom.hydrate(<App page={initialData.page} />, document.getElementById('root'));
