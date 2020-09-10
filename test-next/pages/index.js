1f5321fef2d9a6b286a17d7b3af98174b66f1351
luke <luke@wemakeprice.com>
2020-09-10 10:47:25 +0900

    nextjs 셋업

---
+++ test-next/pages/index.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div>
+      <p>this is main</p>
+      <a href="page1">link</a>
+    </div>
+  )
+}
+export default Main;
---
+++ test-next/pages/page1.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import Icon from '../static/icon.png';
+
+function Page1() {
+  return (
+    <div>
+      <p>this is page1</p>
+      <img src={Icon} />
+      <Head>
+        <title>page1</title>
+      </Head>
+      <Head>
+        <meta name="description" content="hello page1" />
+      </Head>
+      <style jsx>
+        {
+          `p {
+            color: blue;
+            font-size: 18pt;
+          }`
+        }
+      </style>
+    </div>
+  )
+}
+export default Page1;
